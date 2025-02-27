d9a3fc14ac233d475bf18011201a8ea7a55bf63c
あるぱか <www722p@gmail.com>
2025-02-28 00:39:29 +0900

    Update auto.js

--- auto.js
+++ auto.js
@@ -2,17 +2,11 @@ let penSize;
 // 0~30の整数
 let color;
 // 0~FFFFFFのカラーコードを10進数にした整数
-let startX;
-// 0~320のX座標
-let startY;
-// 0~320のY座標
-let endX;
-// 0~320のX座標
-let endY;
-// 0~320のY座標
+let strokeXY = [];
+// 描画するXYの配列
 
 let i = io.connect(Object.keys(io.sockets).slice(0,24));
 // 描画を送信するwss
 
-i.emit('stroke send', penSize, color, 1, [[startX,startY],[endX,endY]]);
+i.emit('stroke send', penSize, color, 1, str);
 // 描画の送信
