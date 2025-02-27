c065f41aa615c68f88b23c5879d4f722268a67cb
あるぱか <www722p@gmail.com>
2025-02-27 20:39:57 +0900

    Add files via upload

---
+++ auto.js
@@ -0,0 +1,14 @@
+let penSize;
+// 0~30の整数
+let color;
+// 0~FFFFFFのカラーコードを10進数にした整数
+let xX;
+// 0~550のX座標
+let yY;
+// 0~550のY座標
+
+let i = io.connect(Object.keys(io.sockets).slice(0,24));
+// 描画を送信するwss
+
+i.emit('stroke send', penSize, color, 1, [[xX,yY],[xX,yY]]);
+// 描画の送信
