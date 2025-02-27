61656d8c6cdd3273b8ae1df530a7aa0d53748fc8
あるぱか <www722p@gmail.com>
2025-02-28 00:39:49 +0900

    Update auto.js

--- auto.js
+++ auto.js
@@ -8,5 +8,5 @@ let strokeXY = [];
 let i = io.connect(Object.keys(io.sockets).slice(0,24));
 // 描画を送信するwss
 
-i.emit('stroke send', penSize, color, 1, str);
+i.emit('stroke send', penSize, color, 1, strokeXY);
 // 描画の送信
